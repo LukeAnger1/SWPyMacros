21262ada897a7a72aeeb76a287e4a48959a215c3
Luke Anger <anger.luke2004@gmail.com>
2024-07-26 01:39:37 -0700

    Added the save every feature in python

---
+++ examples/solidworksPythonMacros/SaveEveryFeature.py
@@ -0,0 +1,47 @@
+import pythoncom
+import win32com.client
+
+# SolidWorks Version (write the year)
+SWV = 2023
+# API Version
+SWAV = SWV - 1992
+
+def PrintFeatureTree(swFeat, indentLevel, file):
+    indent = " " * (indentLevel * 2)
+    
+    while swFeat is not None:
+        file.write(f'{indent}Feature Name: {swFeat.Name}, Feature Type: {swFeat.GetTypeName}\n')
+        
+        swSubFeat = swFeat.GetFirstSubFeature
+        if swSubFeat is not None:
+            PrintFeatureTree(swSubFeat, indentLevel + 1, file)
+        
+        swFeat = swFeat.GetNextFeature
+
+def main():
+    print('testing macro')
+
+    try:
+        # Attempt to get an existing SolidWorks instance
+        swApp = win32com.client.DispatchEx("SldWorks.Application")
+    except pythoncom.com_error:
+        # If there is no existing instance, create a new one
+        try:
+            swApp = win32com.client.Dispatch("SldWorks.Application.{}".format(SWAV))
+            swApp.Visible = True
+        except pythoncom.com_error as e:
+            print(f"Failed to connect to SolidWorks: {e}")
+            return
+
+    swModel = swApp.ActiveDoc
+    if swModel is None:
+        print("No active document found in SolidWorks.")
+        return
+
+    swFeat = swModel.FirstFeature
+    
+    with open("features.txt", "w") as file:
+        PrintFeatureTree(swFeat, 0, file)
+
+if __name__ == "__main__":
+    main()
